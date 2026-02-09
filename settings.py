e1f1530f83de24e7a529bda08f732a9d3d5fa166
toestedbwead <leielcano@gmail.com>
2026-02-10 01:55:19 +0800

    Test prod

--- settings.py
+++ settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'your-secret-key-change-this-in-production'
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['levy-personal-portfolio.onrender.com', 'localhost']
 
